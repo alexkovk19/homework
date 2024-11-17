3c5015e0f9a1ce60b9a747a69821693a7bbfddb3
alexkovk19 <alexkovk@gmail.com>
2024-11-17 19:29:28 +0300

    17/11/2024

--- main.py
+++ main.py
@@ -15,4 +15,42 @@ if __name__ == '__main__':
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-print("Hello world")
+print("Hello, World!")
+# 17/11/2024
+print('Практическое задание по уроку "Базовые структуры данных"')
+
+# "1st program":
+print('Задача 1 (просто) "Арифметика":')
+print("Результат:", (9**0.5*5))
+
+# "2nd program":
+print('Задача 2 (просто) "Логика":')
+print("Результат:", (9.99>9.98 and 1000!=1000.1))
+
+# "3rd program":
+print('Задача 3 (средне) "Школьная загадка":')
+# print("2x2+2 =", (2*2+2))
+# print("2x(2+2) =", (2*(2+2)))
+print("Результат:", (2*2+2)==(2*(2+2)))
+
+# старая задача №3:
+# print(int((1234/10%100))+int((5678/10%100)))
+
+# "4th program":
+print('Задача 4 (сложно) "Первый после точки":')
+print('Какая цифра стоит первая после запятой в числе 123,456 ?')
+# print(type('123.456'))
+# print(float(str('123.456')))
+# print(int(float(str('123.456'))*10)%10)
+print("Результат:", int(float(str('123.456'))*10)%10)
+
+# старая задача №4:
+# print(type('13.42'))
+# print(type('42.13'))
+# print(float(str('13.42')))
+# print(float(str('42.13')))
+# print(int(float(str('13.42'))/1))
+# print(int(float(str('42.13'))*100%100))
+# print(int(float(str('13.42'))/1)==int(float(str('42.13'))*100%100) or int(float(str('42.13'))/1)==int(float(str('13.42'))*100%100))
+
+
