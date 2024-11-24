07c1e732cd5d76a7e8351c1c3bd76955bf0031e9
alexkovk19 <alexkovk@gmail.com>
2024-11-24 18:47:05 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,36 @@
+print ('!!! Практическая работа по уроку "Динамическая типизация"')
+# 24/11/2024 15:10
+
+name = 'Алексей'
+print('Name:', name )
+age = 38
+print('Age:', age )
+new_age = (age + 1)
+print('New_age:', new_age)
+Is_Student = True
+print('Is_Student:', True)
+
+print ('!!! Практическое задание по теме "Переменные"')
+
+# Number_of_completed_homework_assignments = "Количество выполненных домашних заданий"
+Number_of_assignments = 12
+# Number_of_hours_spent = "Количество затраченных часов"
+Number_of_hours = 1.5
+# Course_name = "Название курса"
+Course_name = "Python"
+# Time_for_one_task = "Время на одно задание"
+Time_for_task = Number_of_hours/Number_of_assignments
+print("Курс:", Course_name,",","затрачено часов:", Number_of_hours,",","среднее время выполнения:", Time_for_task,"часа.")
+
+print ('!!! Практическое задание по уроку "Строки и индексация строк"')
+
+example = 'Victor'
+print(example [0])
+print(example [-1])
+print(example [3:])
+print(example [::-1])
+print(example [1::2])
+
+
+
+
---
+++ module_1_4.py
@@ -0,0 +1,11 @@
+
+print ('!!! Практическая работа по уроку "Организация программ и методы строк"')
+# 24/11/2024
+
+my_string = str(input('Ваши Фамилия Имя Отчество: '))
+print ('Количество символов: ', len(my_string))
+print (my_string.upper())
+print (my_string.lower())
+print (my_string.replace(' ', ''))
+print (my_string [0])
+print (my_string [-1])
